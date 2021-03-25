b6b2840cc76ec1904bd2ab761959f1984918e968
Dhiman Sarker Bappi <53374747+dhimanda@users.noreply.github.com>
2021-03-25 22:50:29 +0600

    Add files via upload

---
+++ reg.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>RMSTU BDPC 2021</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400|Roboto:300,400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400|Roboto:300,400,700">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/dhiman/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/dhiman/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/dhiman/avicon-16x16.png">
+    <link rel="manifest" href="site.webmanifest">
+
+</head>
+
+<body>
+	<?php  require_once('header.php'); ?>
+
+	<?php  require_once('nav2.php'); ?>
+
+
+
+  <?php  require_once('contest.php'); ?>
+
+
+
+
+
+  <div class="container">
+    <script src="https://cdn.logwork.com/widget/countdown.js"></script>
+    <a href="https://logwork.com/countdown-2msy" class="countdown-timer" data-style="circles" data-timezone="Asia/Dhaka" data-date="2021-05-03 10:00" data-background="#199e34" data-unitscolor="#000000">RMSTU BDPC 2021 - 3rd May 2021</a>
+  <br>
+  <hr>
+  </div>
+
+
+  <?php  require_once('step.php'); ?>
+<div class="container" id="dhimansb">
+ <hr>
+ <br>
+</div>
+  <div class="container" id="ff-compose"></div>
+<script async defer src="https://formfacade.com/include/107550787769227892989/form/1FAIpQLSez9hlEsHsyivombSJoWAkpVkC8QlxM1EII0txrLXuv4hHFVw/bootstrap.js?div=ff-compose"></script>
+
+
+
+
+<div class="container">
+    <!-- Start: Pretty Footer -->
+    <footer>
+        <div class="row">
+            <div class="text-center col-sm-6 col-md-4 col-xl-4 offset-xl-0 footer-navigation">
+                <a href="index.php"></a>
+                <img class="img-fluid" src="assets/img/RMSTU_logo-03_200.png?h=5e01108fa5eec84f0c7689f802f72659">
+                </a>
+                <p class="links"><a href="index.php">Home</a><strong> · </strong><a href="#">Notice</a><strong> · </strong><a href="facultyFull.php">Faculty</a><strong> · </strong><a href="#">About</a><strong> · </strong><a href="#">Faq</a><strong> · </strong><a href="#">Contact</a></p>
+
+
+            </div>
+            <div class="col-sm-6 col-md-4 footer-contacts">
+                <div><span class="fa fa-map-marker footer-contacts-icon"> </span>
+                    <p><span class="new-line-span">Department of <br> Computer Science and Engineering</p>
+                    <p><span class="new-line-span b">Ranagamati Science &amp; Technology University</span> Jhagrabil, Rangamati , Bangladesh.</p>
+                </div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3683.1024764810963!2d92.16261651427446!3d22.612648137199642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xbbcd72b9a5dd011!2sRangamati%20Science%20and%20Technology%20University!5e0!3m2!1sen!2sbd!4v1614683302740!5m2!1sen!2sbd" width="100%" height="180px" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+            </div>
+            <div class="clearfix"></div>
+            <div class="col-md-4 footer-about">
+
+
+<div class="social-links social-icons">
+  <a href="#"><i class="fa fa-facebook"></i></a>
+  <a target="_blank" href="https://www.youtube.com/channel/UCey4_LOPA5UvMbmZblTGtVw"><i class="fa fa-youtube"></i></a>
+  <a href="#"><i class="icon ion-ios-email"></i></a>
+  <a href="#"><i class="fa fa-phone"></i></a>
+</div>
+
+
+
+            </div>
+        </div>
+        <!-- Start: footer-copyright_bar -->
+        <footer class="footer" style="padding: 0px;">
+            <!-- Start: copyright footer bar  (year shown dynamic) -->
+            <div class="d-flex flex-grow-1 copy-right_text">
+                <div class="container-fluid"><div class="footer_border"></div>
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <p class="copy_right text-center">Copyright © Department of Computer Science and Engineering , RMSTU || All rights reserved. <br><br> Developed by <a target="_blank" style="color:#5DB2FF" href="https://www.facebook.com/Dhimanda">Dhiman Sarker Bappi</a> under the supervision of <a style="color:#5DB2FF"  target="_blank"  href="https://www.facebook.com/sajib.tripura.5"> SAJIB TRIPURA</a>.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End: copyright footer bar  (year shown dynamic) -->
+        </footer>
+        <!-- End: footer-copyright_bar -->
+    </footer>
+    <!-- End: Pretty Footer -->
+</div>
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/script.min.js?h=064f3650469bc433238f07acf0e20392"></script>
+</body>
+
+</html>
---
+++ step.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Prerequirments for registratration.</h2>
+  <p>Please follow this steps to easier experience for registratration. </p>
+
+  <ul class="nav nav-tabs">
+    <li class="active"><a data-toggle="tab" href="#home">Step 1 : Team Info</a></li>
+    <li><a data-toggle="tab" href="#menu1">Step 2 : Persoanl Info</a></li>
+    <li><a data-toggle="tab" href="#menu2">Step 3 : Payment </a></li>
+    <li><a data-toggle="tab" href="#menu3">Step 4 : Submit</a></li>
+  </ul>
+
+  <div class="tab-content" style=" border-color: #ddd ; border-style: solid ; border-width: 0px 0.5px 0.5px 0.5px; padding: 5px">
+    <div id="home" class="tab-pane fade in active">
+      <h3>Step 1 : Team Info</h3>
+      <p>Please ready deside your team name , coach and mailing address for seinding kits.</p>
+    </div>
+    <div id="menu1" class="tab-pane fade">
+      <h3>Step 2 : Persoanl Info</h3>
+      <p>Please keep ready your 3 members basic info like Name , email , mobile number, t-Shirt size etc.</p>
+    </div>
+    <div id="menu2" class="tab-pane fade">
+      <h3>Step 3 : Payment</h3>
+      <p>Please keep 1500BDT in your bKash/Nagad wallet or Send through Sonali Bank account.
+        <br>
+        <b>Keep your transaction id for application form. </b><br>
+        <b style="color:red">Put your team name in the reference section. </b>
+        <br><br>
+        <img height="100%" width="100%" src="assets/img/pay.jpg"></img>
+       </p>
+    </div>
+    <div id="menu3" class="tab-pane fade">
+      <h3>Step 4 : Submit</h3>
+      <p>Now you can submit  your form . We recommend pay first and keep your transaction ID. </p>
+      <a href="#dhimansb">This Form</a>
+    </div>
+  </div>
+  <hr>
+</div>
+
+</body>
+</html>
